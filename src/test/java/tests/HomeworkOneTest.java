32c2604a05bd6a494d7d2146248d2d6a80b277c2
alexandr <butakovalexandr@mail.ru>
2022-10-21 17:10:04 +0600

    fixed run encod and parallels

--- src/test/java/tests/HomeworkOneTest.java
+++ src/test/java/tests/HomeworkOneTest.java
@@ -29,6 +29,7 @@ public class HomeworkOneTest {
         driver = new RemoteWebDriver(
                 URI.create("http://127.0.0.1:4444/wd/hub").toURL(),
                 capabilities
+
         );
 
         driver.manage().window().maximize();
